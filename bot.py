31e2f9298a71fc53d1365f571aa942e340134bd4
fedsmet27-max <fedsmet27@mail.ru>
2026-05-29 12:27:51 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,7 +82,7 @@ def echo_all(message):
     except Exception as e:
         print(f"Ошибка отправки: {e}")
 
-if __main__ == "__name__":
+if __main__ == __name__:
     print("Бро запущен...")
     while True:
         try:
